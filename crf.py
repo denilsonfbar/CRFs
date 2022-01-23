293c4b414489b931e3eda43fa6db405efeef8321
Denilson Fagundes Barbosa <denilsonfbar@gmail.com>
2022-01-23 12:24:47 -0300

    Fixed matrix inicializations

--- crf.py
+++ crf.py
@@ -1,72 +1,58 @@
 import numpy as np
 from models import * 
 
-verbose = True  # Print the detailed execution log
-
-# Converting the probabilities of model to log in base 2
-I = np.log2(I)  # Initial probabilities
-A = np.log2(A)  # Transition matrix
-B = np.log2(B)  # Emission matrix
+verbose = True  # print the detailed execution informations
 
 
 ### HMM FEATURE FUNCTION
-
 class f_HMM:
     
     w = 1.0  # weight of feature function
 
+    ## Return the log2 value of feature function of a time t of a sequence x
+    #
     def f(t,y_t,y_t_minus_1,x):  # feature function
-
         emission_prob = B[y_t,x[t]]
-        
         if t == 0:
-            return emission_prob + I[y_t]
+            return np.log2(emission_prob * I[y_t])
         else:
-            return emission_prob + A[y_t_minus_1,y_t]
+            return np.log2(emission_prob * A[y_t_minus_1,y_t])
 
 
 ### FEATURES FUNCTIONS LIST
-
 ff_list = []  
 ff_list.append(f_HMM)
 
-## Return the weight log sum of features functions results of a time t of sequence x
+
+## Return the weighted log sum of features functions results of a time t of a sequence x
 #
-def psi(t,y_t,y_t_minus_1,x):
-    log_sum_features = 0
-    for ff in ff_list:
-        log_factor = ff.w * ff.f(t,y_t,y_t_minus_1,x)
-        if log_sum_features == 0:  # log(0) is indetermined
-            log_sum_features = log_factor
+def log_sum_features(t,y_t,y_t_minus_1,x):
+    for i in range(len(ff_list)):
+        weighted_log_factor = ff_list[i].w * ff_list[i].f(t,y_t,y_t_minus_1,x)
+        if i == 0:
+            log_sum = weighted_log_factor
         else:
-            log_sum_features = np.logaddexp2(log_sum_features, log_factor) 
-    return log_sum_features
+            log_sum = np.logaddexp2(log_sum, weighted_log_factor) 
+    return log_sum
+
 
 ## Return the Viterbi path
 #
 def viterbi(x):
-
     n_positions = x.size
-    viterbi_matrix = np.zeros((n_states,n_positions))
-    viterbi_paths = np.zeros((n_states,n_positions))
+    viterbi_matrix = np.full((n_states,n_positions), np.NINF)
+    viterbi_paths = np.full((n_states,n_positions), -1)
 
     for t in range(n_positions):
-
         for j in range(n_states):
-    
-            position_probs = np.zeros(n_states)
-
+            log_position_probs = np.full((n_states), np.NINF)
             for i in range(n_states):
-
-                position_probs[i] = psi(t,j,i,x)
-
+                log_position_probs[i] = log_sum_features(t,j,i,x)
                 if t != 0: 
-                    position_probs[i] += viterbi_matrix[i,t-1]
-                
-            viterbi_matrix[j,t] = np.amax(position_probs)
-
+                    log_position_probs[i] += viterbi_matrix[i,t-1]
+            viterbi_matrix[j,t] = np.amax(log_position_probs)
             if t != 0: 
-                viterbi_paths[j,t-1] = np.argmax(position_probs)
+                viterbi_paths[j,t-1] = np.argmax(log_position_probs)
     
     # Finishing the paths:
     for j in range(n_states):
